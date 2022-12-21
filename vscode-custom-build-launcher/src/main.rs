a9576970857d9e0099694675b27805b1e17d0252
Cherryleafroad <13651622+MolotovCherry@users.noreply.github.com>
2022-12-21 10:49:30 -0800

    Properly handle drive root

--- vscode-custom-build-launcher/src/main.rs
+++ vscode-custom-build-launcher/src/main.rs
@@ -24,14 +24,41 @@ Options:
   -h, --help         print this help
   -t, --target <VAL> custom target build directory
 
-Required:
-  DIR                the initial directory used for setting the custom build directory"
+Positional:
+  DIR                the initial directory used for setting the custom build directory.
+                     if this is omitted, it will start vscode without any specific directory"
     );
 }
 
 fn get_hashed_dir(dir: &str) -> String {
     assert!(dir.len() > 0);
     assert!(!dir.ends_with(".."));
+
+    let path = Path::new(dir);
+
+    if !path.try_exists().expect("error reading path") {
+        panic!("path not found");
+    }
+
+    let base_name;
+
+    // Properly handle a drive letter vs regular path
+    let components = path.components().collect::<Vec<_>>();
+    if components.len() <= 2 {
+        let drive = components[0]
+            .as_os_str()
+            .to_str()
+            .expect("Unable to convert to string");
+
+        base_name = drive.replace(":", "");
+    } else {
+        base_name = Path::new(dir)
+            .file_name()
+            .expect("Not a valid path")
+            .to_str()
+            .expect("Failed to convert to string")
+            .to_string();
+    }
 
     // Initial prime and offset chosen for 32-bit output
     // See https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
@@ -48,12 +75,6 @@ fn get_hashed_dir(dir: &str) -> String {
         hash ^= *octet as u64;
         hash = hash * FNV_PRIME % POWER;
     }
-
-    let base_name = Path::new(dir)
-        .file_name()
-        .unwrap()
-        .to_str()
-        .expect("Failed to convert to string");
 
     format!("{base_name}-{hash}")
 }
@@ -91,22 +112,18 @@ fn main() {
 
     // the rust source code directory, which we use to calculate the value to append to the base directory
     let dir = opts.positionals().take(1).collect::<SmallVec<[_; 1]>>();
+    let dir = dir.get(0);
 
-    if let None = dir.get(0) {
-        print_help();
-        return;
+    let mut command = Command::new("code");
+
+    if let Some(dir) = dir {
+        let hashed_dir = get_hashed_dir(dir);
+
+        let build_dir = target_dir.join(&hashed_dir);
+        let build_dir = build_dir.to_str().unwrap();
+
+        command.env("CARGO_BUILD_TARGET_DIR", build_dir).arg(dir);
     }
 
-    let dir = dir[0];
-
-    let hashed_dir = get_hashed_dir(dir);
-
-    let build_dir = target_dir.join(&hashed_dir);
-    let build_dir = build_dir.to_str().unwrap();
-
-    Command::new("code")
-        .env("CARGO_BUILD_TARGET_DIR", build_dir)
-        .arg(dir)
-        .spawn()
-        .expect("Failed to launch VsCode");
+    command.spawn().expect("Failed to launch VsCode");
 }
